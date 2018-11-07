64fbe9530ea8c9412e42560b7330dff8827bdeb1
Kieren Beckett <mail@kierenb.net>
2018-11-07 15:09:09 +0000

    increase gcs http connection pool size

--- flat_application.py
+++ flat_application.py
@@ -29,21 +29,9 @@ from app.storage.storage_encryption import StorageEncryption
 
 storage_backend = os.getenv('EQ_STORAGE_BACKEND')
 if storage_backend == 'gcs':
-    class MyAuthorizedSession(AuthorizedSession):
-        def __init__(self, credentials, max_pool_connections=10, **kwargs):
-            super(MyAuthorizedSession, self).__init__(credentials, **kwargs)
-
-            auth_request_session = requests.Session()
-
-            retry_adapter = requests.adapters.HTTPAdapter(max_retries=3, pool_maxsize=max_pool_connections)
-            auth_request_session.mount("https://", retry_adapter)
-
-            self._auth_request = Request(auth_request_session)
-
-    client = storage.Client(_http=MyAuthorizedSession(
-        google.auth.default()[0],
-        max_pool_connections=int(os.getenv('EQ_GCS_MAX_POOL_CONNECTIONS', '30'))
-    ))
+    gcs_session = AuthorizedSession(google.auth.default()[0])
+    gcs_session.mount('https://', requests.adapters.HTTPAdapter(pool_maxsize=int(os.getenv('EQ_GCS_MAX_POOL_CONNECTIONS', '30'))))
+    client = storage.Client(_http=gcs_session)
     gcs_bucket = client.get_bucket(os.getenv('EQ_GCS_BUCKET_ID'))
 
     def get_answers(user_id, key):
